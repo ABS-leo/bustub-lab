66c7d70a42d20084d812b4cfcd431e80a42f8ebd
ABS-leo <149231074+ABS-leo@users.noreply.github.com>
2025-12-08 22:06:26 +0800

    Add files via upload

---
+++ src/include/storage/index/index_iterator.h
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+//                         CMU-DB Project (15-445/645)
+//                         ***DO NO SHARE PUBLICLY***
+//
+// Identification: src/include/index/index_iterator.h
+//
+// Copyright (c) 2018, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+/**
+ * index_iterator.h
+ * For range scan of b+ tree
+ */
+#pragma once
+#include "storage/page/b_plus_tree_leaf_page.h"
+
+namespace bustub {
+
+#define INDEXITERATOR_TYPE IndexIterator<KeyType, ValueType, KeyComparator>
+
+// 前向声明
+INDEX_TEMPLATE_ARGUMENTS
+class BPlusTreeLeafPage;
+
+INDEX_TEMPLATE_ARGUMENTS
+class IndexIterator {
+ public:
+  // you may define your own constructor based on your member variables
+  IndexIterator();
+  IndexIterator(BufferPoolManager *bpm, BPlusTreeLeafPage<KeyType, ValueType, KeyComparator> *leaf, int index);
+  ~IndexIterator();  // NOLINT
+
+  auto IsEnd() -> bool;
+
+  auto operator*() -> const MappingType &;
+
+  auto operator++() -> IndexIterator &;
+
+  auto operator==(const IndexIterator &itr) const -> bool;
+
+  auto operator!=(const IndexIterator &itr) const -> bool;
+
+ private:
+  // add your own private member variables here
+  BufferPoolManager *buffer_pool_manager_;
+  BPlusTreeLeafPage<KeyType, ValueType, KeyComparator> *leaf_;
+  int index_;
+  page_id_t page_id_;
+};
+
+}  // namespace bustub
